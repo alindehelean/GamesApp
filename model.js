05b0c423cb6add4dcc9cf406566036a7a2df3216
Alin Dehelean <alyndehelean@gmail.com>
2020-03-30 15:40:10 +0300

    encode parameters before sent to API

--- model.js
+++ model.js
@@ -1,145 +1,141 @@
-var apiURL = "https://games-world.herokuapp.com"; // heroku blocu . games world ii etaju . ce ii dupa com is camere in bloc
-                    // dupa fiecare  / devine mai specific
+var apiURL = "https://games-world.herokuapp.com";
 
-    fetch(apiURL + "/games", {
-        method: "GET",
-        headers: {
-            "Content-Type":"application/x-www-form-urlencoded"
-        }
-    }).then(function(response){
-        return response.json();
-    }).then(function(arrayOfGames){ // jsoneresponse ii aray
-        //console.log("the response  ", arrayOfGames );
+fetch(apiURL + "/games", {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+}).then(function(response){
+    return response.json();
+}).then(function(arrayOfGames){
+    //console.log("the response   ", arrayOfGames);
 
-       var container = document.querySelector('.container'); 
+    var container1 = document.querySelector('.container');
 
-        // for(var i=0; i < arrayOfGames.length; i++){
-        //     //console.log(arrayOfGames[i]);
+    //console.log(container1);
 
-        // const h1 = document.createElement("h1");
-        // const p = document.createElement("p");
-        // const img = document.createElement("img");
+    // for(var i = 0; i < arrayOfGames.length; i++) {
+    //     //console.log(arrayOfGames[i]);
+    //     const h1 = document.createElement("h1");
+    //     const p = document.createElement("p");
+    //     const img = document.createElement("img");
 
-        // h1.innerHTML = arrayOfGames[i].title;
-        // p.innerHTML = arrayOfGames[i].description;
+    //     h1.innerHTML = arrayOfGames[i].title;
+    //     p.innerHTML = arrayOfGames[i].description;
 
-        // img.setAttribute("scr", arrayOfGames[i].imageUrl);
+    //     img.setAttribute("src", arrayOfGames[i].imageUrl);
 
-        // container.appendChild(h1);
-        // container.appendChild(img);
-        // container.appendChild(p);
+    //     container.appendChild(h1);
+    //     container.appendChild(img);
+    //     container.appendChild(p);
+    // }
 
-        // arrayOfGames[i].title
-        // arrayOfGames[i].description
-        // arrayOfGames[i].imageUrl
-
-        let gameElements = "";
-
-        for(var i=0; i < arrayOfGames.length; i++){
-            //  gameElements += "<h1>"+ arrayOfGames[i].title +"</h1>" + // gEl += gEl + h1 ...
-            //                     "<img src='" + arrayOfGames[i].imageUrl + "' />" +
-            //                     "<p>" + arrayOfGames[i].description + "</p>" +
-            //                     "<button class='delete-btn' "+
-            //                         " onclick=\"deleteGame('" + arrayOfGames[i]._id + "')\">Delete</button>";
-        
-            // acelasi cod se poate scrie asa :
+    let gameElements = "";
+    for(var i = 0; i < arrayOfGames.length; i++) {
+        // gameElements += "<h1>" + arrayOfGames[i].title + "</h1>" + 
+        //                     "<img src='" + arrayOfGames[i].imageUrl + "' />" +
+        //                     "<p>" + arrayOfGames[i].description + "</p>" + 
+        //                     "<button class='delete-btn' "+
+        //                         " onclick=\"deleteGame('" + arrayOfGames[i]._id + "')\">Delete</button>";
 
         gameElements += `<h1>${arrayOfGames[i].title}</h1> 
-        <img src="${arrayOfGames[i].imageUrl}" />
-        <p>${arrayOfGames[i].description}</p> 
-        <button class="delete-btn" onclick="deleteGame('${arrayOfGames[i]._id}')">Delete</button>`;
+                            <img src="${arrayOfGames[i].imageUrl}" />
+                            <p>${arrayOfGames[i].description}</p> 
+                            <button class="delete-btn" onclick="deleteGame('${arrayOfGames[i]._id}')">Delete</button>`;
 
-        }
-        container.innerHTML= gameElements;
-    });
-
-    function deleteGame(gameID){
-
-        fetch(apiURL + "/games/" + gameID,{
-            method: "DELETE"
-        }).then(function(r){
-            return r.text();
-        }).then(function(apiresponse){
-            console.log(apiresponse)
-        });    
-
-    
     }
-
-    document.querySelector('.submit-Btn').addEventListener("click", function(event){
-        event.preventDefault();
-
-        const gameTitle = document.getElementById("gameTitle");
-        const gameDescription = document.getElementById("gameDescription");
-        const gameGenre = document.getElementById("gameGenre");
-        const gamePublisher = document.getElementById("gamePublisher");
-        const gameImageUrl = document.getElementById("gameImageUrl");
-        const gameRelease = document.getElementById("gameRelease");
-
-        
-            validateFormElement(gameTitle, "The title is required");
-            validateFormElement(gameGenre, "The genre is required");
-            validateFormElement(gameImageUrl, "The image URL is required");
-            validateFormElement(gameRelease, "The release date is required");
-
-            validateReleaseTimeStampElement(gameRelease, "The release date is required");
-       
-
-        if(gameTitle.value !== "" && gameGenre.value !== "" && gameImageUrl.value !== "" && gameRelease.value === ""){
-            const requestParams = {
-                title: gameTitle.value,
-                releaseDate: gameRelease.value,
-                genre: gameGenre.value,
-                publisher: gamePublisher.value,
-                imageURL : gameImageUrl.value,
-                description: gameDescription.value
-    };
-    createGameRequest(requestParams);
-}
-
+    container1.innerHTML = gameElements;
 });
 
-    function validateFormElement(inputElement, errorMessage){
-        if(inputElement.value === ""){
-            if(!document.querySelector('[rel="'+inputElement.id+'"]')){
-                buildErrorMessage(inputElement, errorMessage);
 
-            }
-        } else {
-            if(document.querySelector('[rel="'+inputElement.id+'"]')){
-                console.log("The error is erased!");
-                document.querySelector('[rel="'+inputElement.id+'"]').remove();
-                inputElement.classList.remove("inputError");
-            }
-        }
-    };
+function deleteGame(gameID) {
+    //console.log("delete the game ", gameID);
 
-    function validateReleaseTimeStampElement(inputElement, errorMessage){
-        if(isNaN(inputElement.value) && inputElement.value !== ""){
+    fetch(apiURL + "/games/" + gameID, {
+        method: "DELETE"
+    }).then(function(r){
+        return r.text();
+    }).then(function(apiresponse){
+        console.log(apiresponse);
+        location.reload(true);
+    });
+
+}
+
+document.querySelector(".submit-Btn").addEventListener("click", function(event){
+    event.preventDefault();
+
+    const gameTitle = document.getElementById("gameTitle");
+    const gameDescription = document.getElementById("gameDescription");
+    const gameGenre = document.getElementById("gameGenre");
+    const gamePublisher = document.getElementById("gamePublisher");
+    const gameImageUrl = document.getElementById("gameImageUrl");
+    const gameRelease = document.getElementById("gameRelease");
+
+    validateFormElement(gameTitle, "The title is required!");
+    validateFormElement(gameGenre, "The genre is required!");
+    validateFormElement(gameImageUrl, "The image URL is required!");
+    validateFormElement(gameRelease, "The release date is required!");
+
+    validateReleaseTimestampElement(gameRelease, "The release date you provided is not a valid timestamp!");
+
+
+    if(gameTitle.value !== "" && gameGenre.value !== "" && gameImageUrl.value !== "" && gameRelease.value !== "") {
+     
+
+        var urlencoded = new URLSearchParams();
+        urlencoded.append("title", gameTitle.value);
+        urlencoded.append("releaseDate", gameRelease.value);
+        urlencoded.append("genre", gameGenre.value);
+        urlencoded.append("publisher", gamePublisher.value);
+        urlencoded.append("imageUrl", gameImageUrl.value);
+        urlencoded.append("description", gameDescription.value);
+
+        createGameRequest(urlencoded);
+    }
+})
+
+function validateFormElement(inputElement, errorMessage){
+    if(inputElement.value === "") {
+        if(!document.querySelector('[rel="' + inputElement.id + '"]')){
             buildErrorMessage(inputElement, errorMessage);
-
+        }
+    } else {
+        if(document.querySelector('[rel="' + inputElement.id + '"]')){
+            console.log("the error is erased!");
+            document.querySelector('[rel="' + inputElement.id + '"]').remove();
+            inputElement.classList.remove("inputError");
         }
     }
+}
 
-    function buildErrorMessage(inputEl, errorMsg){
-            inputEl.classList.add("inputError");
-            const errorMsgElement = document.createElement("span");
-            errorMsgElement.setAttribute("rel", inputEl.id);
-            errorMsgElement.classList.add("errorMsg");
-            errorMsgElement.innerHTML = errorMsg;
-            inputEl.after(errorMsgElement);
-
+function validateReleaseTimestampElement(inputElement, errorMessage){
+    if(isNaN(inputElement.value) && inputElement.value !== "") {
+        buildErrorMessage(inputElement, errorMessage);
     }
-    
+}
 
-    function createGameRequest(gameObject){
-        fetch(apiURL + "/games", {
-            method:"POST",
-            headers: {
-                "Content-Type":"application/x-www-form-urlencoded"
-            },
-            body: JSON.stringify(gameObject)
-        });
-    };
+function buildErrorMessage(inputEl, errosMsg){
+    inputEl.classList.add("inputError");
+    const errorMsgElement = document.createElement("span");
+    errorMsgElement.setAttribute("rel", inputEl.id);
+    errorMsgElement.classList.add("errorMsg");
+    errorMsgElement.innerHTML = errosMsg;
+    inputEl.after(errorMsgElement);
+}
+
+function createGameRequest(gameObject){
+    fetch(apiURL + "/games", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: gameObject
+    }).then(function(response){
+        return response.text();
+    }).then(function(createdGame){
+        console.log(createdGame);
+    });
+}
 
 
